af3ef9885397bc85fb2c2030169a4afd83688208
VitorMarinelli <vitorFmarinelli@protonmail.com>
2025-10-07 16:35:34 -0300

    troquei o texto de baixo

--- my-app/src/app/page.tsx
+++ my-app/src/app/page.tsx
@@ -21,7 +21,7 @@ export default function Home() {
             .
           </li>
           <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
+            Eu alterei o texto
           </li>
         </ol>
 
